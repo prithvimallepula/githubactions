1e205383824ce399b4f084dfab60ed8595960ad0
Prithvi Mallepula <prithvimallepula@MacBook-Pro.local>
2023-06-30 14:10:11 -0400

    Updated test_website URLs

--- test_website.py
+++ test_website.py
@@ -14,7 +14,12 @@ def main():
         "https://pinknoiseplaylisting.com/how-it-works",
         "https://pinknoiseplaylisting.com/for-curators",
         "https://pinknoiseplaylisting.com/contact",
-        "https://pinknoiseplaylisting.com/album-art-generator"
+        "https://pinknoiseplaylisting.com/album-art-generator",
+        "https://pinknoiseplaylisting.com/track-compatibility/1lRtH4FszTrwwlK5gTSbXO",
+        "https://pinknoiseplaylisting.com/next-steps",
+        "https://docs.google.com/forms/u/1/d/1atqmfm-lxYGnIuMaUpsmdlcczEFV0_1WmQucKxj5KxM/edit?ts=63d81540",
+        "https://docs.google.com/forms/u/1/d/1rV8M82qiEv6ZpCkcFW27_ELYE-o5XkG8CZ0d_uAqdx0/edit?usp=forms_home&ths=true",
+        "https://pinknoiseplaylisting.com/track-compatibility/1lRtH4FszTrwwlK5gTSbXO"
         # Add more URLs to test
     ]
 
